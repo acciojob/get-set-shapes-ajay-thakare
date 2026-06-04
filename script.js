e88a96ef2f449a3c272f0bdd9301aca0f8a10e1d
Ajay Thakare <143386761+ajay-thakare@users.noreply.github.com>
2026-06-04 21:02:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 //complete this code
 class Rectangle {
 	constructor(width, height){
-		this.width = width;
-		this.height = height;
+		this._width = width;
+		this._height = height;
 	}
 
 	get width(){
-		return this.width;
+		return this._width;
 	}
 
 	get height(){
-		return this.height;
+		return this._height;
 	}
 
 	getArea(){
@@ -24,7 +24,7 @@ class Square extends Rectangle {
 	}
 
 	getPerimeter() {
-        return 4 * this.width;
+        return 4 * this._width;
     }
 }
 
